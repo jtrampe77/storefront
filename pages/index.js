f1b355500eb4314baa150bd95ef69eab7d0fde93
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2022-03-29 11:48:09 -0600

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
 
 import PageTitle from "../components/PageTitle/PageTitle"
 
-export default function Home() {
-  return (
-   <PageTitle title="StoreFront" tagline="Featured Products"/>
+
+function Product({title,...props}){
+  return(
+    <li>{title}</li>
   )
+}
+
+export default function Home() {
+  
+  // product data
+  // Building UI from data
+  // map inside the return implicit arrow function
+
+  const productData = [{name:"Nike", uid:"1"}, {name:"Yeezy", uid:"2"},{name:"Adidas", uid:"3"},]
+
+  
+
+   return (
+     <>
+       <PageTitle title="StoreFront" tagline="Featured Products"/>
+       <Product title="Sneaker" />
+       {
+         productData.map(({uid,name,...product})=> <Product key={uid} title={name}/>)
+       }
+     </>
+ 
+  )
+
 }
