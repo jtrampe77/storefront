b86955b459b2bb96ea1f76217dd99c96a21f9180
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2022-04-13 11:03:06 -0600

    changes

---
+++ pages/api/checkout.js
@@ -0,0 +1,36 @@
+import { getProduct } from "./../../libs/getProduct";
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  const product = await getProduct(req.body.uid);
+  if(product){
+    if (req.method === 'POST') {
+        
+      try {
+        const session = await stripe.checkout.sessions.create({
+              line_items: [
+            {
+              name:product.productName,
+              description: product.productDescription,
+              images:[product.imageUrl],
+              amount: product.productPrice*100,
+              currency:"CAD",
+              quantity:1
+  
+            },
+          ],
+          mode: 'payment',
+          success_url: `${req.headers.origin}/?success=true`,
+          cancel_url: `${req.headers.origin}/?canceled=true`,
+        });
+        res.redirect(303, session.url);
+      } catch (err) {
+        res.status(err.statusCode || 500).json(err.message);
+      }
+    } else {
+      res.setHeader('Allow', 'POST');
+      res.status(405).end('Method Not Allowed');
+    }
+   }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
+import { loadStripe } from '@stripe/stripe-js';
 import Head from 'next/head';
 import PageTitle from "../components/PageTitle/PageTitle"
 import ProductCard from "../components/ProductCard/ProductCard"
 
 
+
+
 export default function Home(props) {
   
    const products = props.products
+
+  //  console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+  //  console.log(process.env.STRIPE_SECRET_KEY)
+
+   const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 
    return (
      <>
@@ -13,9 +21,9 @@ export default function Home(props) {
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>StoreFront</title>
+        <title>SneakerHeadz</title>
      </Head>
-        <PageTitle title="Storefront" tagline="our store" />
+        <PageTitle title="SneakerHeadz" tagline="Get the best sneakerz out there" />
         <main>
             {
               products.map(product => <ProductCard key={product.uid} product={product} />)
